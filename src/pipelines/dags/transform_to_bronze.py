e9cbfc284fe537296b32f2d308102505236a9841
Alex Gi <alex@alexgi.alexgi.com>
2026-04-29 21:17:25 +0700

    update code

--- src/pipelines/dags/transform_to_bronze.py
+++ src/pipelines/dags/transform_to_bronze.py
@@ -1,1 +1,2 @@
 # some stuffs code
+# This is the DAG file for transforming data from raw to bronze layer in the data lakehouse architecture. It defines the tasks and their dependencies for the transformation process.
