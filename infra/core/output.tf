426dab21356c0d133a99143af77667377f9426b8
Alex Gi <alex@alexgi.alexgi.com>
2026-04-29 13:40:21 +0700

    configuration of airflow on EC2

--- infra/core/output.tf
+++ infra/core/output.tf
@@ -34,3 +34,7 @@ output "ec2_public_ip" {
   description = "EC2 public IP address"
   value       = aws_instance.airflow_ec2.public_ip  
 }
+output "ec2_instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.airflow_ec2.id
+}
